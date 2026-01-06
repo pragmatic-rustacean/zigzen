ad552b5f5eba64107b70e0626a73aae45a45a553
Awesome Rustacean <geniusinrust@gmail.com>
2026-01-06 13:16:58 +0300

    if with boolean conditions

--- src/main.zig
+++ src/main.zig
@@ -4,7 +4,11 @@ pub fn main() void {
     const a: i32 = 4;
     const b: i32 = 5;
 
-    const result = if (a < b) 19 else 10;
-
-    std.debug.print("My name is: {} \n", .{result});
+    if (a != b) {
+        std.debug.print("Naah, they aren't equal", .{});
+    } else if (a == 9) {
+        std.debug.print("Look at that they are equal", .{});
+    } else {
+        std.debug.print("Nasty comparision", .{});
+    }
 }

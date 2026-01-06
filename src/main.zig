b4ea6faac3396e0a9688a91b6196623ce71f4c43
Awesome Rustacean <geniusinrust@gmail.com>
2026-01-06 13:10:47 +0300

    if and else if

--- src/main.zig
+++ src/main.zig
@@ -3,8 +3,10 @@ const std = @import("std");
 pub fn main() void {
     const age = 22;
     if (age < 18) {
-        std.debug.print("You are an underage yooh. Wait until you get to 18", .{});
+        std.debug.print("Yooh, you baby. Wait and grow up.", .{});
+    } else if (age < 20 and age >= 18) {
+        std.debug.print("You can enter but, head to the kids section", .{});
     } else {
-        std.debug.print("Welcome to the party, big dog.", .{});
+        std.debug.print("Welcome big dog. This place deserves you", .{});
     }
 }

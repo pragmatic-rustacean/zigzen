697654d973a27316d4b84b3a5ea3e153161c4f4c
Awesome Rustacean <geniusinrust@gmail.com>
2026-01-06 13:39:34 +0300

    Iterating over multiple collections: Because one isn’t challenging enough

--- src/main.zig
+++ src/main.zig
@@ -2,10 +2,11 @@ const std = @import("std");
 
 pub fn main() void {
     const items = [_]i32{ 1, 2, 3, 5, 6 };
+    const more = [_]i32{ 9, 8, 7, 6, 5 };
     var sum: i32 = 0;
 
-    for (items, 0..) |_, index| {
-        sum += @intCast(index);
+    for (items, more) |i, j| {
+        sum += i + j;
     }
 
     std.debug.print("The sum is: {}\n", .{sum});

2f7b32594edef60909c97eed27005e8f58ef98ae
Awesome Rustacean <geniusinrust@gmail.com>
2026-01-06 13:29:22 +0300

    for loop

--- src/main.zig
+++ src/main.zig
@@ -1,28 +1,15 @@
 const std = @import("std");
 
 pub fn main() void {
-    const today = 6;
-    switch (today) {
-        1 => {
-            std.debug.print("Today is a new year", .{});
-        },
-        2 => {
-            std.debug.print("Today is after new year", .{});
-        },
-        3 => {
-            std.debug.print("Now the year is old", .{});
-        },
-        4 => {
-            std.debug.print("Today is a week after new year", .{});
-        },
-        5 => {
-            std.debug.print("Now today is on a monday", .{});
-        },
-        6 => {
-            std.debug.print("Today is on a Tuesday and I didn't go to school", .{});
-        },
-        else => {
-            std.debug.print("Naah, not what you are looking for", .{});
-        },
+    const items = [_]i32{ 1, 2, 3, 5, 6 };
+    var sum: i32 = 0;
+
+    for (items) |item| {
+        if (item == 0) {
+            continue;
+        }
+        sum += item;
     }
+
+    std.debug.print("The sum is: {}\n", .{sum});
 }

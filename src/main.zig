d5855f8b11fd8c9d0e29520a3f6c87b58e576c27
Awesome Rustacean <geniusinrust@gmail.com>
2026-01-06 13:14:11 +0300

    if as an expression

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,10 @@
 const std = @import("std");
 
 pub fn main() void {
-    const age = 22;
-    if (age < 18) {
-        std.debug.print("Yooh, you baby. Wait and grow up.", .{});
-    } else if (age < 20 and age >= 18) {
-        std.debug.print("You can enter but, head to the kids section", .{});
-    } else {
-        std.debug.print("Welcome big dog. This place deserves you", .{});
-    }
+    const a: i32 = 4;
+    const b: i32 = 5;
+
+    const result = if (a < b) 19 else 10;
+
+    std.debug.print("My name is: {} \n", .{result});
 }

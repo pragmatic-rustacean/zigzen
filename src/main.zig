95b29b0c7fdf5cd1d98212dad17b33135a451fb5
Awesome Rustacean <geniusinrust@gmail.com>
2026-01-06 13:32:33 +0300

    Accessing indexes in for loop

--- src/main.zig
+++ src/main.zig
@@ -4,11 +4,8 @@ pub fn main() void {
     const items = [_]i32{ 1, 2, 3, 5, 6 };
     var sum: i32 = 0;
 
-    for (items) |item| {
-        if (item == 0) {
-            continue;
-        }
-        sum += item;
+    for (items, 0..) |_, index| {
+        sum += @intCast(index);
     }
 
     std.debug.print("The sum is: {}\n", .{sum});

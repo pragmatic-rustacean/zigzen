ef51eef29f8d3beaf337b37aeb9cbf6734e08487
Awesome Rustacean <geniusinrust@gmail.com>
2026-01-07 13:32:00 +0300

    Enums: When you’re tired of magic numbers ruining your day

--- src/main.zig
+++ src/main.zig
@@ -1,25 +1,40 @@
 const std = @import("std");
 
+const day = enum {
+    monday,
+    tuesday,
+    wednesday,
+    thursday,
+    friday,
+    saturday,
+    sunday,
+};
+
+const http = enum(i32) {
+    Ok = 200,
+    NotFound = 404,
+    InternalServerError = 500,
+    BadRequest = 400,
+};
+
+const trafic_light = enum {
+    Red,
+    Green,
+    Yellow,
+
+    pub fn is_red(self: trafic_light) bool {
+        return self == .Red;
+    }
+};
+
 pub fn main() void {
-    light: switch (@as(u8, 1)) {
-        1 => {
-            std.debug.print("Light is red, cars stops\n", .{});
-            continue :light 2;
-        },
-        2 => {
-            std.debug.print("Light is yellow, cars get ready\n", .{});
-            continue :light 3;
-        },
-        3 => {
-            std.debug.print("Light is green, cars go\n", .{});
-            continue :light 4;
-        },
-        4 => {
-            std.debug.print("Pedestrian crossing activated, cars stop\n", .{});
-            return;
-        },
-        else => {
-            std.debug.print("Naah dude, let's meet at the traffic dept\n", .{});
-        },
+    const today = day.wednesday;
+    const day_number = @intFromEnum(today);
+    const status_code = http.Ok;
+    const code = @intFromEnum(status_code);
+    std.debug.print("Status code: {}\n", .{code});
+
+    if (today == .wednesday) {
+        std.debug.print("Today is hump day. day number {}!\n", .{day_number + 1});
     }
 }
